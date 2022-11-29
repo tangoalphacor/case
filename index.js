3e3d14d7bc94300fef8d31d622bcf979cefb0f51
Abhinavkannan <abhinavt20006@gmail.com>
2022-11-29 23:10:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+fetch("https://jsonplaceholder.typicode.com/todos").then((data)=>{
+// console.log(data); json format
+return data.json(); //converted to object
+}).then((objectdata)=>{
+    console.log(objectdata[0].title)
+    let tabledata=""
+    objectdata.map((values)=>{
+        tabledata +=`<tr>
+        <td>${values.id}</td>
+        <td>${values.title}</td>
+        <td><input class="form-check-input" type="checkbox" value=""id="flexCheckDefault" ></input>${values.completed}</td>
+        </tr> `
+    })
+    document.getElementById("tb").innerHTML=tabledata
+    let check=document.getElementById("flexCheckDefault").checked
+    if(check&&values.completed=="true"){
+        alert("Task complete")
+    }
+})
+
+    
+ 
