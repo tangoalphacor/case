8027031103e1e5cb448d1c86e01cc81c952b4748
Abhinavkannan <abhinavt20006@gmail.com>
2022-11-30 09:44:37 +0530

    new

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 fetch("https://jsonplaceholder.typicode.com/todos").then((data)=>{
 // console.log(data); json format
 return data.json(); //converted to object
@@ -8,15 +9,10 @@ return data.json(); //converted to object
         tabledata +=`<tr>
         <td>${values.id}</td>
         <td>${values.title}</td>
-        <td><input class="form-check-input" type="checkbox" value=""id="flexCheckDefault" ></input>${values.completed}</td>
+        <td><input class="form-check-input" type="checkbox" value=""id="flexCheckDefault" ></input></td>
         </tr> `
     })
     document.getElementById("tb").innerHTML=tabledata
-    let check=document.getElementById("flexCheckDefault").checked
-    if(check&&values.completed=="true"){
-        alert("Task complete")
-    }
 })
-
     
  
---
+++ login.js
@@ -0,0 +1,12 @@
+let username="admin"
+let pass="12345"
+let user=document.getElementById("user")
+let password=document.getElementById("password")
+function validate(){
+    if(username!=user.value&&pass!=passworod.value){
+       document.getElementById("alp")= false
+       
+    }
+
+}
+
